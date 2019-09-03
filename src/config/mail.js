1a01df9a7334b4c568a21592bfeeaf21d1ecefe3
Matheus Boari <matheusboari@gmail.com>
2019-09-03 20:21:47 -0300

    Config email :wrench:

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'fc0c779d889c27',
+    pass: '0496f401cfe514',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+}
