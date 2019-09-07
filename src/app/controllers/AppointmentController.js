766b23b02c3a469a507aab317831a8b08c75be52
Matheus Boari <matheusboari@gmail.com>
2019-09-06 21:08:23 -0300

    Add Redis :sparkles:

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -7,7 +7,8 @@ import User from '../models/User'
 import File from '../models/File'
 import Notification from '../schemas/Notification'
 
-import Mail from '../../lib/Mail'
+import CancellationMail from '../jobs/CancellationMail'
+import Queue from '../../lib/Queue'
 
 class AppointmentController {
   async index(req, res) {
@@ -119,6 +120,11 @@ class AppointmentController {
           as: 'provider',
           attributes: ['name', 'email'],
         },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
       ],
     })
 
@@ -143,10 +149,8 @@ class AppointmentController {
 
     await appointment.save()
 
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento cancelado',
-      html: 'Você tem um novo cancelamento',
+    await Queue.add(CancellationMail.key, {
+      appointment,
     })
 
     return res.json(appointment)
